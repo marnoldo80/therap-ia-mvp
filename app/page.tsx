e395609177a01d62c6a4ad251c9c73a126875eda
marnoldo80 <marnoldo80@gmail.com>
2025-10-21 09:17:49 +0000

    Step 2: Next.js creato + home

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main style={{maxWidth:680,margin:"40px auto",padding:20}}>
+      <h1>Therap-IA MVP</h1>
+      <p>Home OK. Prosegui all’iscrizione terapeuta (step 3):</p>
+      <p><a href="/signup-terapeuta">/signup-terapeuta</a></p>
+    </main>
+  );
+}
